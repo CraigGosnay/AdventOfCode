6a2af0bb0fffc703eb9f3a24da0e421242b2178c
Craig Gosnay <craig.gosnay@gmail.com>
2020-12-09 22:01:40 -0500

    complete q1

--- day_one/reportrepair.py
+++ day_one/reportrepair.py
@@ -5,7 +5,7 @@ def parse_input(filepath: str) -> list:
 
     return [int(i) for i in expenses]
 
-def find_sum(expenses: list) -> int:
+def find_sum_p1(expenses: list) -> int:
 
     s = set()
     for i in expenses:
@@ -16,6 +16,27 @@ def find_sum(expenses: list) -> int:
             return i*j
         s.add(i)
     return 0
+
+def find_sum_p2(expenses: list) -> int:
+
+    target = 2020
+    expenses.sort() # O(n log n)
+
+    # O(n^2)
+    for i in range(len(expenses)):
+        curr = expenses[i]
+        lo = i + 1
+        hi = len(expenses) - 1
+
+        while lo < hi and expenses[lo] < target:
+            if curr + expenses[lo] + expenses[hi] == target:
+                return curr*expenses[lo]*expenses[hi]
+            elif expenses[lo] + expenses[hi] + curr> target:
+                hi -= 1
+            elif expenses[lo] + expenses[hi] + curr < target:
+                lo += 1
     
 if __name__ == "__main__":
-    print(find_sum(parse_input('aoc_input.txt')))
+    exps = parse_input('aoc_input.txt')
+    print(find_sum_p1(exps))
+    print(find_sum_p2(exps))
